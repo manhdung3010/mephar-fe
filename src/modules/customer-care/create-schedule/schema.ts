d27b0019efe9ac826031e36af6e73699dd5ce33d
haibh <haibh.dev@gmail.com>
2024-07-18 14:09:07 +0700

    fixbug trip

--- src/modules/customer-care/create-schedule/schema.ts
+++ src/modules/customer-care/create-schedule/schema.ts
@@ -3,8 +3,10 @@ import * as yup from "yup";
 export const schema = yup.object().shape({
   name: yup.string().required("Đây là trường bắt buộc!"),
   time: yup.string().required("Đây là trường bắt buộc!"),
-  lat: yup.number().required("Đây là trường bắt buộc!"),
-  lng: yup.number().required("Đây là trường bắt buộc!"),
+  lat: yup.string(),
+  lng: yup.string(),
+  latEnd: yup.string(),
+  lngEnd: yup.string(),
   listCustomer: yup
     .array(
       yup.object({
