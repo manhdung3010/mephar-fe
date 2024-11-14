3c0602eda20779efe75c1496352d2ded089284cf
haibh <haibh.dev@gmail.com>
2024-11-14 10:21:00 +0700

    fix description

--- src/modules/products/list-product/row-detail/Info.tsx
+++ src/modules/products/list-product/row-detail/Info.tsx
@@ -15,14 +15,9 @@ import MarketBlueIcon from "@/assets/marketBlue.svg";
 import { CustomButton } from "@/components/CustomButton";
 import UpdateStatusModal from "@/components/CustomModal/ModalUpdateStatusItem";
 import { CustomUnitSelect } from "@/components/CustomUnitSelect";
-import {
-  EProductStatus,
-  EProductStatusLabel,
-  EProductType,
-  getEnumKeyByValue,
-} from "@/enums";
+import { EProductStatus, EProductStatusLabel, EProductType, getEnumKeyByValue } from "@/enums";
 import { formatMoney, getImage, hasPermission } from "@/helpers";
-import parse from 'html-react-parser';
+import parse from "html-react-parser";
 import type { IProduct } from "../types";
 import DeleteProductModal from "./DeleteProduct";
 import PrintBarcodeModal from "./PrintBarcodeModal";
@@ -31,29 +26,23 @@ import { useRecoilValue } from "recoil";
 import { profileState } from "@/recoil/state";
 import { RoleAction, RoleModel } from "@/modules/settings/role/role.enum";
 
-const Info = ({
-  record,
-  onChangeUnit,
-}: {
-  record: IProduct;
-  onChangeUnit: any;
-}) => {
+const Info = ({ record, onChangeUnit }: { record: IProduct; onChangeUnit: any }) => {
   const queryClient = useQueryClient();
   const router = useRouter();
   const profile = useRecoilValue(profileState);
 
   const [openPrintBarcodeModal, setOpenPrintBarcodeModal] = useState(false);
   const [openDeleteProductModal, setOpenDeleteProductModal] = useState(false);
-  const [openUpdateProductStatusModal, setOpenUpdateProductStatusModal] =
-    useState(false);
+  const [openUpdateProductStatusModal, setOpenUpdateProductStatusModal] = useState(false);
   const [showFullDescription, setShowFullDescription] = useState(false);
 
   const toggleDescription = () => {
     setShowFullDescription(!showFullDescription);
   };
 
-  const { mutate: mutateDeleteProduct, isLoading: isLoadingDeleteProduct } =
-    useMutation(() => deleteProduct(Number(record.id)), {
+  const { mutate: mutateDeleteProduct, isLoading: isLoadingDeleteProduct } = useMutation(
+    () => deleteProduct(Number(record.id)),
+    {
       onSuccess: async () => {
         await queryClient.invalidateQueries(["LIST_PRODUCT"]);
         setOpenDeleteProductModal(false);
@@ -61,18 +50,15 @@ const Info = ({
       onError: (err: any) => {
         message.error(err?.message);
       },
-    });
+    },
+  );
 
   const onSubmit = () => {
     mutateDeleteProduct();
   };
 
-  const {
-    mutate: mutateUpdateStatusProduct,
-    isLoading: isLoadingUpdateStatusProduct,
-  } = useMutation(
-    (payload: { status: EProductStatus }) =>
-      updateProductStatus(Number(record.id), payload),
+  const { mutate: mutateUpdateStatusProduct, isLoading: isLoadingUpdateStatusProduct } = useMutation(
+    (payload: { status: EProductStatus }) => updateProductStatus(Number(record.id), payload),
     {
       onSuccess: async () => {
         await queryClient.invalidateQueries(["LIST_PRODUCT"]);
@@ -81,7 +67,7 @@ const Info = ({
       onError: (err: any) => {
         message.error(err?.message);
       },
-    }
+    },
   );
 
   const onSubmitUpdateStatus = (status: EProductStatus) => {
@@ -114,12 +100,10 @@ const Info = ({
 
   return (
     <>
-      <div className="bg-[#F5F5F5] p-4 text-lg font-medium text-[#0070F4]">
-        {record?.name}
-      </div>
+      <div className="bg-[#F5F5F5] p-4 text-lg font-medium text-[#0070F4]">{record?.name}</div>
       <div className="flex gap-12">
         <div className="flex flex-col gap-5 p-5">
-          {(record?.image?.path || record?.imageUrl) ? (
+          {record?.image?.path || record?.imageUrl ? (
             <Image
               width={235}
               height={235}
@@ -130,9 +114,7 @@ const Info = ({
           ) : (
             <div className="h-[235px] w-[235px]" />
           )}
-          {record?.barCode && (
-            <Barcode className=" mx-auto h-[110px]" value={record?.barCode} />
-          )}
+          {record?.barCode && <Barcode className=" mx-auto h-[110px]" value={record?.barCode} />}
         </div>
         <div className="grid flex-1 grid-cols-2 gap-4">
           <div className="grid grid-cols-2 gap-5">
@@ -144,14 +126,12 @@ const Info = ({
             <div className="text-gray-main">Mã vạch:</div>
             <div className="text-black-main">{record?.barCode}</div>
           </div>
-          {
-            record?.type === EProductType.MEDICINE && (
-              <div className="grid grid-cols-2 gap-5">
-                <div className="text-gray-main">Mã thuốc:</div>
-                <div className="text-black-main">{record?.drugCode}</div>
-              </div>
-            )
-          }
+          {record?.type === EProductType.MEDICINE && (
+            <div className="grid grid-cols-2 gap-5">
+              <div className="text-gray-main">Mã thuốc:</div>
+              <div className="text-black-main">{record?.drugCode}</div>
+            </div>
+          )}
           <div className="grid grid-cols-2 gap-5">
             <div className="text-gray-main">Nhóm hàng: </div>
             <div className="text-black-main">{record?.groupProduct?.name}</div>
@@ -159,11 +139,7 @@ const Info = ({
           <div className="grid grid-cols-2 gap-5">
             <div className="text-gray-main">Loại hàng:</div>
             <div className="text-black-main">
-              {record?.type === 1
-                ? "Thuốc"
-                : record?.type === 2
-                  ? "Hàng hóa"
-                  : "Combo - đóng gói"}
+              {record?.type === 1 ? "Thuốc" : record?.type === 2 ? "Hàng hóa" : "Combo - đóng gói"}
             </div>
           </div>
           <div className="grid grid-cols-2 gap-5">
@@ -180,27 +156,21 @@ const Info = ({
           </div>
           <div className="grid grid-cols-2 gap-5">
             <div className="text-gray-main">Giá vốn:</div>
-            <div className="text-black-main">
-              {formatMoney(record?.primePrice)}
-            </div>
+            <div className="text-black-main">{formatMoney(record?.primePrice)}</div>
           </div>
           <div className="grid grid-cols-2 gap-5">
             <div className="text-gray-main">Trọng lượng:</div>
             <div className="text-black-main">{record?.weight}</div>
           </div>
-          {
-            record?.type === EProductType.MEDICINE && (
-              <div className="grid grid-cols-2 gap-5">
-                <div className="text-gray-main">Đường dùng:</div>
-                <div className="text-black-main">{record?.productDosage?.name}</div>
-              </div>
-            )
-          }
+          {record?.type === EProductType.MEDICINE && (
+            <div className="grid grid-cols-2 gap-5">
+              <div className="text-gray-main">Đường dùng:</div>
+              <div className="text-black-main">{record?.productDosage?.name}</div>
+            </div>
+          )}
           <div className="grid grid-cols-2 gap-5">
             <div className="text-gray-main">Vị trí:</div>
-            <div className="text-black-main">
-              {record?.productPosition?.name}
-            </div>
+            <div className="text-black-main">{record?.productPosition?.name}</div>
           </div>
           {/* <div className="grid grid-cols-2 gap-5">
             <div className="text-gray-main">Cảnh báo hết hạn:</div>
@@ -208,35 +178,27 @@ const Info = ({
               {record?.warningExpiryDate || record?.warningExpiryText}
             </div>
           </div> */}
-          {
-            record?.type === EProductType.MEDICINE && (
-              <div className="grid grid-cols-2 gap-5">
-                <div className="text-gray-main">Số đăng ký:</div>
-                <div className="text-black-main">{record?.registerNumber}</div>
-              </div>
-            )
-          }
-          {
-            record?.type === EProductType.MEDICINE && (
-              <div className="grid grid-cols-2 gap-5">
-                <div className="text-gray-main">Hoạt chất:</div>
-                <div className="text-black-main">{record?.activeElement}</div>
-              </div>
-            )
-          }
-          {
-            record?.type === EProductType.MEDICINE && (
-              <div className="grid grid-cols-2 gap-5">
-                <div className="text-gray-main">Hàm lượng:</div>
-                <div className="text-black-main">{record?.content}</div>
-              </div>
-            )
-          }
+          {record?.type === EProductType.MEDICINE && (
+            <div className="grid grid-cols-2 gap-5">
+              <div className="text-gray-main">Số đăng ký:</div>
+              <div className="text-black-main">{record?.registerNumber}</div>
+            </div>
+          )}
+          {record?.type === EProductType.MEDICINE && (
+            <div className="grid grid-cols-2 gap-5">
+              <div className="text-gray-main">Hoạt chất:</div>
+              <div className="text-black-main">{record?.activeElement}</div>
+            </div>
+          )}
+          {record?.type === EProductType.MEDICINE && (
+            <div className="grid grid-cols-2 gap-5">
+              <div className="text-gray-main">Hàm lượng:</div>
+              <div className="text-black-main">{record?.content}</div>
+            </div>
+          )}
           <div className="grid grid-cols-2 gap-5">
             <div className="text-gray-main">Hãng sản xuất:</div>
-            <div className="text-black-main">
-              {record?.productManufacture?.name}
-            </div>
+            <div className="text-black-main">{record?.productManufacture?.name}</div>
           </div>
           <div className="grid grid-cols-2 gap-5">
             <div className="text-gray-main">Nước sản xuất:</div>
@@ -244,16 +206,12 @@ const Info = ({
           </div>
           <div className="grid grid-cols-2 gap-5">
             <div className="text-gray-main">Quy cách đóng gói:</div>
-            <div className="text-black-main">
-              {record?.packingSpecification}
-            </div>
+            <div className="text-black-main">{record?.packingSpecification}</div>
           </div>
           <div className="grid grid-cols-2 gap-5">
             <div className="text-gray-main">Mô tả:</div>
             <div className="text-black-main">
-              <div className={showFullDescription ? "" : "line-clamp-5"}>
-                {parse(record?.description)}
-              </div>
+              <div className={showFullDescription ? "" : "line-clamp-5"}>{parse(record?.description ?? "")}</div>
               {record?.description?.length > 240 && (
                 <button onClick={toggleDescription} className="text-blue-500">
                   {showFullDescription ? "Thu gọn" : "Xem thêm"}
@@ -274,62 +232,30 @@ const Info = ({
                   record?.productUnit[0]?.id
                 }
               /> */}
-              <ListUnit
-                data={record?.productUnit}
-                onChangeUnit={onChangeUnit}
-                record={record}
-                isDetailOpen={true}
-              />
+              <ListUnit data={record?.productUnit} onChangeUnit={onChangeUnit} record={record} isDetailOpen={true} />
             </div>
           </div>
           <div className="grid grid-cols-2 gap-5">
             <div className="text-gray-main">Trạng thái:</div>
-            <div
-              className={`${record?.status === EProductStatus.active
-                ? "text-[#00B63E]"
-                : "text-gray-main"
-                }`}
-            >
-              {
-                EProductStatusLabel[
-                getEnumKeyByValue(EProductStatus, record?.status)
-                ]
-              }
+            <div className={`${record?.status === EProductStatus.active ? "text-[#00B63E]" : "text-gray-main"}`}>
+              {EProductStatusLabel[getEnumKeyByValue(EProductStatus, record?.status)]}
             </div>
           </div>
         </div>
       </div>
       <div className="flex justify-end gap-4">
-        {hasPermission(
-          profile?.role?.permissions,
-          RoleModel.list_product,
-          RoleAction.update
-        ) && (
-            <CustomButton
-              type={
-                record?.status === EProductStatus.inactive ? "success" : "disable"
-              }
-              outline={true}
-              prefixIcon={
-                record?.status === EProductStatus.inactive ? (
-                  <div></div>
-                ) : (
-                  <Image src={LockGrayIcon} alt="" />
-                )
-              }
-              onClick={() => setOpenUpdateProductStatusModal(true)}
-              disabled={isLoadingUpdateStatusProduct}
-            >
-              {record?.status === EProductStatus.inactive
-                ? "Mở bán"
-                : "Ngưng kinh doanh"}
-            </CustomButton>
-          )}
-        <CustomButton
-          type="primary"
-          outline={true}
-          prefixIcon={<Image src={MarketBlueIcon} alt="" />}
-        >
+        {hasPermission(profile?.role?.permissions, RoleModel.list_product, RoleAction.update) && (
+          <CustomButton
+            type={record?.status === EProductStatus.inactive ? "success" : "disable"}
+            outline={true}
+            prefixIcon={record?.status === EProductStatus.inactive ? <div></div> : <Image src={LockGrayIcon} alt="" />}
+            onClick={() => setOpenUpdateProductStatusModal(true)}
+            disabled={isLoadingUpdateStatusProduct}
+          >
+            {record?.status === EProductStatus.inactive ? "Mở bán" : "Ngưng kinh doanh"}
+          </CustomButton>
+        )}
+        <CustomButton type="primary" outline={true} prefixIcon={<Image src={MarketBlueIcon} alt="" />}>
           Đẩy lên chợ
         </CustomButton>
         <CustomButton
@@ -340,47 +266,31 @@ const Info = ({
         >
           In mã vạch
         </CustomButton>
-        {hasPermission(
-          profile?.role?.permissions,
-          RoleModel.list_product,
-          RoleAction.create
-        ) && (
-            <CustomButton
-              type="primary"
-              outline={true}
-              prefixIcon={<Image src={CopyBlueIcon} alt="" />}
-              onClick={redirectCopy}
-            >
-              Sao chép
-            </CustomButton>
-          )}
+        {hasPermission(profile?.role?.permissions, RoleModel.list_product, RoleAction.create) && (
+          <CustomButton
+            type="primary"
+            outline={true}
+            prefixIcon={<Image src={CopyBlueIcon} alt="" />}
+            onClick={redirectCopy}
+          >
+            Sao chép
+          </CustomButton>
+        )}
 
-        {hasPermission(
-          profile?.role?.permissions,
-          RoleModel.list_product,
-          RoleAction.delete
-        ) && (
-            <CustomButton
-              outline={true}
-              onClick={() => setOpenDeleteProductModal(true)}
-              prefixIcon={<Image src={DeleteRedIcon} alt="" />}
-            >
-              Xoá
-            </CustomButton>
-          )}
-        {hasPermission(
-          profile?.role?.permissions,
-          RoleModel.list_product,
-          RoleAction.update
-        ) && (
-            <CustomButton
-              type="success"
-              prefixIcon={<Image src={EditWhiteIcon} alt="" />}
-              onClick={redirectUpdate}
-            >
-              Cập nhật
-            </CustomButton>
-          )}
+        {hasPermission(profile?.role?.permissions, RoleModel.list_product, RoleAction.delete) && (
+          <CustomButton
+            outline={true}
+            onClick={() => setOpenDeleteProductModal(true)}
+            prefixIcon={<Image src={DeleteRedIcon} alt="" />}
+          >
+            Xoá
+          </CustomButton>
+        )}
+        {hasPermission(profile?.role?.permissions, RoleModel.list_product, RoleAction.update) && (
+          <CustomButton type="success" prefixIcon={<Image src={EditWhiteIcon} alt="" />} onClick={redirectUpdate}>
+            Cập nhật
+          </CustomButton>
+        )}
       </div>
 
       <PrintBarcodeModal
@@ -401,9 +311,7 @@ const Info = ({
         onCancel={() => setOpenUpdateProductStatusModal(false)}
         onSuccess={() =>
           onSubmitUpdateStatus(
-            record?.status === EProductStatus.active
-              ? EProductStatus.inactive
-              : EProductStatus.active
+            record?.status === EProductStatus.active ? EProductStatus.inactive : EProductStatus.active,
           )
         }
         content="sản phẩm"
